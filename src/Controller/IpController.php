cc346e82ac7e5096063fe770ac1babb2e78e3347
peter <perter@126.com>
2020-08-12 17:39:55 +0800

    增加跨域支持

---
+++ src/Controller/IpController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\third\amqp\IpAmqp;
+use by\infrastructure\helper\Object2DataArrayHelper;
+use Dbh\SfCoreBundle\Common\ByEnv;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IpController extends AbstractController
+{
+    /**
+     * @param $ip
+     * @return Response
+     * @throws \ReflectionException
+     */
+    public function index($ip)
+    {
+        if (empty($ip)) {
+            return new JsonResponse("ip缺失");
+        }
+
+        $ret = (new IpAmqp(ByEnv::get("AMQP_KEY")))->get($ip);
+
+        return new JsonResponse(Object2DataArrayHelper::getDataArrayFrom($ret));
+    }
+}
---
+++ src/third/amqp/IpAmqp.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace App\third\amqp;
+
+
+use by\component\http\HttpRequest;
+use by\infrastructure\helper\CallResultHelper;
+
+class IpAmqp
+{
+    protected $key;
+
+    public function __construct($key)
+    {
+        $this->key = $key;
+    }
+
+    public function get($ip)
+    {
+        $url = 'https://restapi.amap.com/v3/ip?';
+        $params = [
+            'ip' => $ip,
+            'output' => "json",
+        ];
+        $sign = $this->sign($params, $this->key);
+        $params['sign'] = $sign;
+        $params['key'] = $this->key;
+        $url .= http_build_query($params);
+
+        $resp = HttpRequest::newSession()->get($url);
+        if (!$resp->success) {
+            return CallResultHelper::fail($resp->getError());
+        }
+        $content = $resp->getBody()->getContents();
+        $content = json_decode($content, JSON_OBJECT_AS_ARRAY);
+        if (is_array($content) && array_key_exists('status', $content)) {
+            if (intval($content['status']) == 0) {
+                return CallResultHelper::fail($content['info'], $content);
+            }
+            if (intval($content['status']) == 1) {
+                $data = [
+                    'province' => $content['province'],
+                    'city' => $content['city'],
+                    'adcode' => $content['adcode'],
+                    'rectangle' => $content['rectangle']
+                ];
+                return CallResultHelper::success($data);
+            }
+        }
+        return CallResultHelper::fail("未知错误");
+    }
+
+    public function sign($params, $key)
+    {
+        ksort($params, SORT_ASC);
+        $sign = "";
+        foreach ($params as $k => $v) {
+            if (strlen($sign) > 0) {
+                $sign .= '&';
+            }
+            $sign .= $k.'='.$v;
+        }
+        $sign .= $key;
+        return md5($sign);
+    }
+}
