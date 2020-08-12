e3038619a03c86211f01cb1ea3361942f52a5a53
peter <perter@126.com>
2020-08-12 18:12:07 +0800

    增加跨域支持

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env'.$_SERVER['APP_ENV'].'.local');
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env.'.$_SERVER['APP_ENV'].'.local');
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
